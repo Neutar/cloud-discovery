5f781f35ed07a0861fd4d47fe9fb42accb182c5e
nurersoyozyurek <nersoy88@gmail.com>
2020-06-07 00:19:32 +0200

    [12] eureka server

---
+++ src/test/java/com/neutar/tecrubesi/clouddiscovery/CloudDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutar.tecrubesi.clouddiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
